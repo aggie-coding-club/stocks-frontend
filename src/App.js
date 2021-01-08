994b3b7fb06ec9397a12e60a7fbe35c990f8e15e
Ryan Hall <ryan.hall.tx@gmail.com>
2021-01-08 17:34:56 -0600

    AppBar and SearchBar mostly finished. Just need a  bit more CSS. Sort of reponsive but not really for mobile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar';
+import SearchBar from './components/SearchBar';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { useState } from 'react';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [darkState, setDarkState] = useState(true);
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          //type: darkState ? 'dark' : 'light'
+        },
+      }),
+      [prefersDarkMode],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <AppBar />
+      </div>
+    </ThemeProvider>
   );
 }
 
