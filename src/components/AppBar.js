a12d3bfe8e1a298d45328fdad14379b195b7593e
Ryan Hall <ryan.hall.tx@gmail.com>
2021-01-08 23:29:20 -0600

    Removing old css styles

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -22,28 +22,6 @@ const useStyles = makeStyles((theme) => ({
       display: 'block',
     },
   },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
