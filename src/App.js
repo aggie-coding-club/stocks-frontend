8715b7e75372a6006d8bcc8dabc7606dc622a38d
Sofia-Ortega <s.ortega@tamu.edu>
2021-01-10 14:47:46 -0600

    started grid for stocks

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Landing from "./components/Landing"
 import './App.css';
 import AppBar from './components/dashboard/AppBar';
 import SearchBar from './components/dashboard/SearchBar';
+import StockCards from './components/dashboard/StockCards';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { useState } from 'react';
+import {SatelliteTwoTone} from "@material-ui/icons";
 
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -22,12 +24,16 @@ function App() {
   );
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="App">
-        <AppBar />
-      </div>
-    </ThemeProvider>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <AppBar />
+        </div>
+
+      </ThemeProvider>
+      <StockCards />
+    </div>
 
   );
 }
