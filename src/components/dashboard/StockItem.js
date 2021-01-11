a00fd5af716deb4ab26c75ac6f4c26bf31859c08
Sofia-Ortega <s.ortega@tamu.edu>
2021-01-10 19:31:48 -0600

    paper styling added to each title

--- src/components/dashboard/StockItem.js
+++ src/components/dashboard/StockItem.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
+const useStyles = makeStyles( (theme) => ({
+  paper: {
+    padding: theme.spacing(3),
+    marginTop: '20px',
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+
+}))
 export default function StockItem({stock}) {
+  const classes = useStyles();
   return(
     <div>
-      <p>{stock.title}</p>
+      <Paper className={classes.paper}>{stock.title}</Paper>
     </div>
   )
 }
