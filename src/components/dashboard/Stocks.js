0d63d1357c7f8e303f76c287e7d00abe559e361a
Nathan Nguyen <nathan.tm.nguyen@gmail.com>
2021-01-24 03:16:30 -0600

    nhwn: toggle cards in the graph

--- src/components/dashboard/Stocks.js
+++ src/components/dashboard/Stocks.js
@@ -2,14 +2,13 @@ import React from 'react';
 import StockItem from './StockItem';
 
 
-export default function Stocks({stocks, delStock, cardClick}) {
+export default function Stocks({stocks, delStock, cardToggle}) {
   return(
     <div>
       { stocks.map((stock) => (
-        <StockItem key={stock.id} stock={stock} delStock={delStock} cardClick={cardClick}/>
+        <StockItem key={stock.id} stock={stock} delStock={delStock} cardToggle={cardToggle}/>
       ))
       }
     </div>
-
-  )
+  );
 }
