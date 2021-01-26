de12c58b05ef01e303656ccf55d45fdcc3daca98
Sofia-Ortega <s.ortega@tamu.edu>
2021-01-25 18:08:21 -0600

    blurb more centered

--- src/components/dashboard/Instructions.js
+++ src/components/dashboard/Instructions.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
-const instrStyle = {
-  margin: 'auto',
-  textAlign: 'right',
-  padding: '30vh 0 12vh 0',
+const rootStyle = {
+  padding: '35vh 0 12vh 0',
 }
 
+const containerStyle = {
 
+  float: 'right',
+  textAlign: 'center'
+}
 export default function Instructions() {
   return(
-    <div style={instrStyle}>
-      <h3>Add your stocks from the search bar</h3>
-      <h3>Toggle some stocks to graph them </h3>
+    <div style={rootStyle}>
+      <div style={containerStyle}>
+        <h3>Add your stocks from the search bar</h3>
+        <h3>Toggle some stocks to graph them </h3>
+      </div>
+
     </div>
   )
 }
